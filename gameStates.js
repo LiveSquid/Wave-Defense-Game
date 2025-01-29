cf4886dc7b935d2839ef76910a579a82b286a82b
LiveSquid <liamashok@gmail.com>
2025-01-29 11:39:26 -0500

    Title

--- gameStates.js
+++ gameStates.js
@@ -47,7 +47,7 @@ export class Menu extends State {
         title.style.top = '15%';
         title.style.transform = 'translate(-50%, -50%)';
         title.style.color = 'white';
-        title.style.fontSize = '8vw'
+        title.style.fontSize = '6vw'
         title.style.whiteSpace = 'nowrap'
 
         subTitle.style.display = 'block';
